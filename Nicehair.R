62f9b3f2edf2a3018be2cfc1e619ccf0961fd10d
Kaellas <pawelgach@midea.pl>
2024-04-08 19:25:39 +0200

    Version 0.1

--- Nicehair.R
+++ Nicehair.R
@@ -3,3 +3,10 @@
 
 # Libraries used:
 
+
+# Reading the data
+data <- read.csv("https://storage.googleapis.com/s360xnicehair_case_competition/nicehair-data/nicehair_data_final.csv")
+
+data
+skimr::skim(data)
+
